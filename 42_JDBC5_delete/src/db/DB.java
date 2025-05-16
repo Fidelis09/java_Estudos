c1b6fe15e6d8b62f9e0d61fdfaec9dff4e91db57
João Fidelis <jfidelis999@gmail.com>
2025-05-16 14:28:42 -0300

    JDBC update

---
+++ 42_JDBC5_delete/src/db/DB.java
@@ -0,0 +1,59 @@
+package db;
+
+import java.sql.*;
+
+public class DB {
+
+    private static Connection conn = null;
+
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/coursejdbc";
+    private static final String USUARIO = "developer";
+    private static final String SENHA = "1234567";
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver"); // <- essa linha é ESSENCIAL
+                conn = DriverManager.getConnection(URL, USUARIO, SENHA);
+            } catch (ClassNotFoundException e) {
+                throw new DbExeption("Driver JDBC não encontrado: " + e.getMessage());
+            } catch (SQLException e) {
+                throw new DbExeption("Erro de conexão: " + e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+
+    public static void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new DbExeption(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeStatement(Statement st){
+        if(st!=null){
+            try {
+                st.close();
+            }catch (SQLException e){
+                throw  new DbExeption(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs){
+        if(rs!=null){
+            try {
+                rs.close();
+            }catch (SQLException e){
+                throw  new DbExeption(e.getMessage());
+            }
+        }
+    }
+
+
+}
